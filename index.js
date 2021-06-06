c4e7995c0c6edb6d2a7696f298e3d82746aba52a
Айдар <bajanga07@gmail.com>
2021-06-06 21:08:04 +0700

    Added db

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const { Pool } = require('pg')
 const PORT = process.env.PORT || 5000
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/db', (req, res)=> {
+    try {
+      const client = await pool.connect();
+      const res = await client.query('SELECT * FROM test_table');
+      const ress = {
+        'results' : (result) ? result.rows : null
+      }
+      res.render('pages/db', ress);
+      client.release();
+    }
+    catch (err) {
+      console.error(err);
+      res.send("Error" + err);
+    }
+  })
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
