362a265fba7113e72d6c4eda733ca42e74b61134
Айдар <bajanga07@gmail.com>
2021-06-06 20:51:08 +0700

    TIMES added

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/times', (req, res) => res.send(showTimes()))
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res)=> res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+showTimes = () => {
+  let res = '';
+  const times = process.env.TIMES || 5;
+  for (let i = 0; i < times; i++) {
+    res += i + ' ';
+  }
+  return res;
+}
